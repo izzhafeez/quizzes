86f1985ba1d9e5340637ba9a399770b3a5803056
Izz Hafeez <izzhafeez@gmail.com>
2023-09-18 01:29:33 +0800

    Edit handleGiveUp.jsx: Add quizzes

---
+++ src/components/quiz/events/handleGiveUp.jsx
@@ -0,0 +1,12 @@
+import getGrade from "../helper/getGrade";
+
+const handleGiveUp = ({ setCompleted, setGrade, setFeatures, itemPositions, correct, features }) => () => {
+  setCompleted(itemPositions);
+  setGrade(getGrade(correct/features.length));
+  setFeatures(features.map(feature => {
+    feature.setStyle(feature.get('style'));
+    return feature;
+  }));
+};
+
+export default handleGiveUp;
