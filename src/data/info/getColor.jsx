7dc5fa6b5b681e11ff580397d6625b0d1926d463
Izz Hafeez <izzhafeez@gmail.com>
2023-09-21 22:58:51 +0800

    Edit getColor.jsx: Add quizzes

--- src/data/info/getColor.jsx
+++ src/data/info/getColor.jsx
@@ -9,4 +9,41 @@ export const getSchoolColor = school => {
     default:
       return 'GREEN';
   }
-}
+};
+
+export const getMRTColor = mrt => {
+  const line = mrt.label.substring(0, 2);
+  switch (line) {
+    case 'EW':
+      case 'CG':
+        return 'GREEN';
+      case 'NS':
+        return 'RED';
+      case 'NE':
+        return 'PURPLE';
+      case 'CC':
+      case 'CE':
+        return 'YELLOW';
+      case 'DT':
+        return 'BLUE';
+      case 'TE':
+        return 'BROWN';
+      case 'JE':
+      case 'JS':
+      case 'JW':
+        return 'CYAN';
+      case 'CP':
+      case 'CR':
+        return 'BRIGHT_GREEN';
+      case 'BP':
+      case 'SW':
+      case 'SE':
+      case 'PW':
+      case 'PE':
+      case 'ST':
+      case 'PT':
+        return 'GREY';
+      default:
+        return 'WHITE';
+  }
+};
