82cd5e7a66bd733496cab2ba265e6c7e5d7d6d18
Izz Hafeez <izzhafeez@gmail.com>
2023-09-21 22:58:51 +0800

    Edit GiveUpButton.jsx: Add quizzes

--- src/components/quiz/inputs/GiveUpButton.jsx
+++ src/components/quiz/inputs/GiveUpButton.jsx
@@ -2,7 +2,7 @@ import handleGiveUp from "../events/handleGiveUp";
 
 const GiveUpButton = (params) => {
   return <p>
-    {`${params.correct}/${params.features.length}`} guessed <button onClick={handleGiveUp(params)} className='btn btn-danger py-1 ms-2'>Give Up</button>
+    {`${params.correct}/${params.features.length}`} guessed <button onClick={handleGiveUp(params)}>Give Up</button>
   </p>
 };
 
