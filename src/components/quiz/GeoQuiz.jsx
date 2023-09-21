9d3da2c3a123f0d21d9eeb55d94e2031a14824fc
Izz Hafeez <izzhafeez@gmail.com>
2023-09-21 22:58:50 +0800

    Edit GeoQuiz.jsx: Add quizzes

--- src/components/quiz/GeoQuiz.jsx
+++ src/components/quiz/GeoQuiz.jsx
@@ -1,14 +1,12 @@
 import MapContainer from "components/map/MapContainer";
 import React, { useEffect, useState } from "react";
-import purify from "./helper/purify";
 import GuessInput from "./inputs/GuessInput";
 import GiveUpButton from "./inputs/GiveUpButton";
 import handleReset from "./events/handleReset";
 import getFeature from "./helper/getFeature";
 
-const GeoQuiz = ({ data, getText, getStyle, getColor }) => {
+const GeoQuiz = ({ data, getText, getStyle, getColor, purifyGuess }) => {
   const [features, setFeatures] = useState([]);
-  const [items, setItems] = useState([]);
   const [itemPositions, setItemPositions] = useState({});
   const [completed, setCompleted] = useState({});
   const [correct, setCorrect] = useState(0);
@@ -19,25 +17,24 @@ const GeoQuiz = ({ data, getText, getStyle, getColor }) => {
     handleReset({ setCorrect, setCompleted, setGrade, setRecenter });
     const dataList = Object.entries(data).map(([k, v]) => v)
     const allItems = dataList.map((d, index) => {
-      const text = purify(getText(d));
-      if (!!itemPositions[text]) {
+      const text = purifyGuess(getText(d));
+      if (!!itemPositions[text] && !itemPositions[text].includes(index)) {
         itemPositions[text].push(index);
       } else {
         itemPositions[text] = [index];
       }
-      setItemPositions(itemPositions);
       return d;
     });
-    setItems(allItems);
+    setItemPositions(itemPositions);
 
     const allFeatures = allItems.map(item => {
       return getFeature({ item, getText, getStyle, getColor });
     })
     setFeatures(allFeatures);
-  }, [itemPositions, data, getText, getStyle, getColor]);
+  }, [itemPositions, data, getText, getStyle, getColor, purifyGuess]);
 
-  return <div className='text-start'>
-    {GuessInput({ setRecenter, setCompleted, setCorrect, setFeatures, completed, itemPositions, features, correct })}
+  return <div>
+    {GuessInput({ setRecenter, setCompleted, setCorrect, setFeatures, completed, itemPositions, features, correct, purifyGuess })}
     {GiveUpButton({ correct, features, setCompleted, setGrade, setFeatures, itemPositions })}
     {!!grade && <p><b>Grade:</b><br/>{grade}</p>}
     <MapContainer features={features} willRecenter={willRecenter}/>
