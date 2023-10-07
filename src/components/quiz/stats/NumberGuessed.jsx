6e50d9a0aa7834263f2ec7e58f17dd013d0d6d36
Izz Hafeez <izzhafeez@gmail.com>
2023-10-07 17:39:13 +0800

    Edit NumberGuessed.jsx: Fix quiz

---
+++ src/components/quiz/stats/NumberGuessed.jsx
@@ -0,0 +1,7 @@
+const NumberGuessed = ({ correct }) => {
+  return <p className="my-auto text-ew text-center">
+    <span className="text-3xl">{correct}</span>
+  </p>
+};
+
+export default NumberGuessed;
