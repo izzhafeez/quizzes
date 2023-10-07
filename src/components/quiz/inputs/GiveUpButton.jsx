408f300c9926a41e5d411ab2ed24098bbf1208ea
Izz Hafeez <izzhafeez@gmail.com>
2023-10-07 17:39:14 +0800

    Edit components: Fix quiz

--- src/components/quiz/inputs/GiveUpButton.jsx
+++
@@ -1,9 +0,0 @@
-import handleGiveUp from "../events/handleGiveUp";
-
-const GiveUpButton = (params) => {
-  return <p>
-    {`${params.correct}/${params.features.length}`} guessed <button onClick={handleGiveUp(params)}>Give Up</button>
-  </p>
-};
-
-export default GiveUpButton;
