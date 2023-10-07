c22544a75fd32f55c38a3deba656ecdc49ae3300
Izz Hafeez <izzhafeez@gmail.com>
2023-10-07 17:39:13 +0800

    Edit GuessInput.jsx: Fix quiz

--- src/components/quiz/inputs/GuessInput.jsx
+++ src/components/quiz/inputs/GuessInput.jsx
@@ -1,14 +1,17 @@
 import handleGuess from "../events/handleGuess";
 
 const GuessInput = (params) => {
-  return <div>
-    <label htmlFor='guess'><b>Enter answer here:</b></label>
+  return <div className="grid gap-1">
+    <label htmlFor="guess font-semibold">Enter answer here</label>
     <input
       type='text'
       id='guess'
       name='guess'
+      className='py-1 px-2 border-[1px] w-80 border-lrt-light rounded-md'
+      placeholder='Guess...'
       onChange={handleGuess(params)}
     />
+    <p>{params.features.length} possible answers</p>
   </div>
 };
 
