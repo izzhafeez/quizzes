7cdef950acebe14702d0244605fca1b6f5a51db4
Izz Hafeez <izzhafeez@gmail.com>
2023-10-07 17:39:13 +0800

    Edit index.js: Fix quiz

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router } from 'react-router-dom';
+import 'tailwindcss/tailwind.css'; // Import the Tailwind CSS styles
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
